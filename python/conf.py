6559bec3c9be52c5eef1f417682822f534c31949
Slasher <sejust@163.com>
2013-03-14 22:12:01 +0800

    finish python v1.0

--- python/conf.py
+++ python/conf.py
@@ -1,4 +1,7 @@
 
+
+
+
 project = 'sandbox'
 accesskey = 'SYS0000000000SANDBOX'
 secretkey = '1' * 40
--- python/large_upload.py
+++ python/large_upload.py
@@ -4,22 +4,23 @@
 Date:
 @author
 """
-__author__ = ' '
-__version__ = '1.0.0'
+__author__ = 'Slasher'
+__version__ = '0.1.0'
 
 import os
 import sys
 import time
-import json
 import hashlib
 import pprint
 
 
 import conf
+import pool
 import sinastorageservice as s3
 
 pdir = os.path.dirname( os.path.abspath( sys.argv[0] ) )
-tmpdir = os.path.join( pdir, 'tmp' )
+tempdir = os.path.join( pdir, 'tempfile' )
+partdir = os.path.join( tempdir, 'tmp' )
 
 
 def partedfile( filepath, filesplit = 1024 * 1024 * 4, cipher = 'sha1' ):
@@ -35,7 +36,7 @@ def partedfile( filepath, filesplit = 1024 * 1024 * 4, cipher = 'sha1' ):
         merge = '<?xml version="1.0" encoding="UTF-8"?><CompleteMultipartUpload>'
 
         filename = os.path.basename( filepath )
-        tmpfiledir = os.path.join( tmpdir, filename )
+        tmpfiledir = os.path.join( partdir, filename )
 
         if not os.path.isdir( tmpfiledir ):
             os.makedirs( tmpfiledir, mode = 0755 )
@@ -49,13 +50,13 @@ def partedfile( filepath, filesplit = 1024 * 1024 * 4, cipher = 'sha1' ):
 
             part_md5 = hashlib.md5()
             part_md5.update( part )
-            _md5 = part_md5.hexdigest()
+            hashinfo = part_md5.hexdigest()
 
             with open( os.path.join( tmpfiledir,
-                                     '%d.%s' % ( partnum, _md5 ) ), 'wb' ) as _f:
+                                     '%d.%s' % ( partnum, hashinfo ) ), 'wb' ) as _f:
                 _f.write( part )
 
-            merge += '<Part><PartNumber>%d</PartNumber><ETag>%s</ETag></Part>' % ( partnum, _md5 )
+            merge += '<Part><PartNumber>%d</PartNumber><ETag>%s</ETag></Part>' % ( partnum, hashinfo )
 
             sobj.update( part )
 
@@ -68,107 +69,122 @@ def partedfile( filepath, filesplit = 1024 * 1024 * 4, cipher = 'sha1' ):
         with open( os.path.join( tmpfiledir, '.merge.xml' ), 'w' ) as _f:
             _f.write( merge )
 
-        _md5 = sobj.hexdigest()
+        hashinfo = sobj.hexdigest()
 
-        with open( os.path.join( tmpfiledir, '.md5' ), 'w' ) as _f:
-            _f.write( _md5 )
+        with open( os.path.join( tmpfiledir, '.hashinfo' ), 'w' ) as _f:
+            _f.write( hashinfo )
 
-        return _md5
+        return hashinfo
 
 
-if __name__ == '__main__':
+def upload_large( key, fn = None ):
 
-    errorfile = open( 'log.log', 'a' )
+    f = fn or os.path.join( tempdir, 'bigfile.jpg' )
+    print 'upload : ', f
 
-    project = conf.project
-    accesskey = conf.accesskey
-    secretkey = conf.secretkey
 
-    ffile = os.path.join( pdir, 'bigfile.jpg' )
-    print 'upload : ', ffile
+    handle = s3.S3( conf.accesskey, conf.secretkey, conf.project )
+    handle.set_need_auth()
 
-    bn = os.path.basename( ffile )
-    _md5 = partedfile( ffile, 1024 * 1024 * 1, 'md5' )
+    bn = os.path.basename( f )
+    hashinfo = partedfile( f, 1024 * 1024 * 1, 'sha1' )
 
-    handle = s3.S3( accesskey, secretkey, project )
-
-    key = 'test_upload_bigfile'
-    with open( os.path.join( tmpdir, bn, '.key' ), 'w' ) as _f:
+    with open( os.path.join( partdir, bn, '.key' ), 'w' ) as _f:
         _f.write( key )
 
-    handle.set_need_auth()
-    tf, out = handle.get_upload_idc()
+    domain = handle.get_upload_idc()
+    print 'domain : ',domain
 
-    if not tf:
-        print out
-        sys.exit( 1 )
-    else:
-        domain = out
-
-    print domain
-    with open( os.path.join( tmpdir, bn, '.domain' ), 'w' ) as _f:
+    with open( os.path.join( partdir, bn, '.domain' ), 'w' ) as _f:
         _f.write( domain )
 
     handle.set_domain( domain )
 
     for i in range( 1, 4 ):
-        tf, out = handle.get_upload_id( key )
-        if not tf:
-            print out
-        else:
-            uploadid = out
+        try:
+            uploadid = handle.get_upload_id( key )
             break
+        except Exception, e:
+            print 'get uploadid : ', e
     else:
         sys.exit( 1 )
 
-    with open( os.path.join( tmpdir, bn, '.uploadid' ), 'w' ) as _f:
+    with open( os.path.join( partdir, bn, '.uploadid' ), 'w' ) as _f:
         _f.write( uploadid )
-    print 'uploadId=' + uploadid
+    print 'uploadId : ' + uploadid
+
 
     round = 1
     while True:
         print 'Round %d: ' % ( round, )
         round += 1
-        time.sleep( 1 )
 
-        partnums = []
-        tf, out = handle.list_parts( key, uploadid )
-        if not tf:
-            print out
-            continue
-        else:
-            partnums = out
+        partnums = handle.get_list_parts( key, uploadid )
 
-        print partnums
-        parts = os.listdir( os.path.join( tmpdir, bn ) )
+        parts = os.listdir( os.path.join( partdir, bn ) )
         parts = [ k for k in parts if not k.startswith( '.' ) ]
         parts.sort( key = lambda x : int( x.split( '.' )[0] ), reverse = False )
 
         parts = [ p for p in parts \
-                 if int( p.split( '.' )[0] ) not in  partnums]
+                 if int( p.split( '.' )[0] ) not in partnums]
 
+        #print parts
         if parts == []:
+            print 'all parts are uploaded'
             break
+
+
+        def _upload_part(  key, uploadid, num, fn ):
+
+            handle = s3.S3( conf.accesskey, conf.secretkey, conf.project )
+            handle.set_need_auth()
+            handle.set_domain( domain )
+
+            for i in range( 1, 4 ):
+                try:
+                    out = handle.upload_part( key, uploadid, num, fn )
+                    print 'try %d: upload part %d ok' % ( i, num )
+                    return
+                except Exception, e:
+                    print 'try %d: upload part %d : ' % ( i, num )
+                    continue
+            else:
+                print 'upload part %s failed' % ( num, )
+
+        import pool
+
+        threadpool = pool.WorkerPool( 10 )
+        upload_part = threadpool.runwithpool( _upload_part )
+
 
         for part in parts:
             num, p_md5 = part.split( '.' )
+            num = int( num )
 
-            for i in range( 1, 4 ):
-                tf, out = handle.upload_part( key, uploadid, num, os.path.join( tmpdir, bn, part ) )
+            upload_part( key, uploadid, num, os.path.join( partdir, bn, part ) )
 
-                if tf:
-                    print 'try %d: upload %s ok' % ( i, part )
-                    break
-                else:
-                    print 'try %d: upload %s error %s' % ( i, part, str( out ) )
-            else:
-                pass
+        threadpool.join()
 
-    tf, out = handle.merge_parts( key, uploadid, os.path.join( tmpdir, bn, '.merge.xml' ) )
+        time.sleep( 60 )
 
-    if tf:
-        print 'merger %s OK' % ( key, )
+
+    time.sleep( 10 )
+    for i in range( 1, 4 ):
+        try:
+            out = handle.merge_parts( key, uploadid, os.path.join( partdir, bn, '.merge.xml' ) )
+
+            print 'merger %s ok.' % ( key, )
+
+            break
+        except Exception, e:
+            time.sleep( 2 )
+            print 'try %d: merge : %s' % ( i, repr( e ) )
     else:
-        print out
+        print 'merger %s failed.' % ( key, )
+
+
+if __name__ == '__main__':
+
+    upload_large( 'big_file_upload' )
 
 
