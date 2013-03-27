3585d4ec821c5a4fac98de4ba5c2e4d17621ff1a
Slasher <sejust@163.com>
2013-03-27 19:28:55 +0800

    add sinastorageservice.java

---
+++ java/sinastorage/sinastorageservice.java
@@ -0,0 +1,14 @@
+package sinastorage;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+public class sinastorageservice{
+
+    public sinastorageservice() {
+
+        System.out.println( "sinastorageservice" );
+
+    }
+
+}
